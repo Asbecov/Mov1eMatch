ec05d1f7cb738486e7c77d97136ec79e96c0af1a
Asbecov <m2311505@edu.misis.ru>
2025-05-16 20:14:02 +0300

    feature: fixing action and routing

--- mmm_flutter/lib/main.dart
+++ mmm_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import 'package:mmm/app/movie_match.dart';
 import 'package:mmm/common/constants/app_constants.dart';
@@ -6,6 +7,7 @@ import 'package:mmm_client/mmm_client.dart';
 
 late final Client client;
 void main() {
+  usePathUrlStrategy();
   client = Client(serverUrl);
 
   runApp(const MovieMatch());
