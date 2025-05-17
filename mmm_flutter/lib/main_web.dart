e940c0cd0343c22d1cf93a4672c3f2fc9656c8f0
Asbecov <m2311505@edu.misis.ru>
2025-05-17 19:31:12 +0300

    fix: fixed web build

--- mmm_flutter/lib/main_web.dart
+++ mmm_flutter/lib/main_web.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+import 'package:mmm/main.dart';
+
 import 'package:mmm/app/movie_match.dart';
 import 'package:mmm/common/constants/app_constants.dart';
 import 'package:mmm_client/mmm_client.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-late final Client client;
 void main() {
   usePathUrlStrategy();
   client = Client(serverUrl);
